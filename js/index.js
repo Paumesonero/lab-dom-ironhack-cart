61b4c33977eea4a3857620294c7e04f520454e9d
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-05-02 19:51:35 +0200

    lab done it 1-3

--- js/index.js
+++ js/index.js
@@ -2,23 +2,43 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  //step 1
+  const price = product.querySelector('.price span')
+  const quantity = product.querySelector('input')
+  //Step 2
+  let priceValue = price.innerHTML;
+  let quantityValue = quantity.value
+  //Step 3
+  let subtotalValue = (priceValue * quantityValue).toFixed(2);
+  //Step 4
+  const subtotalDisplay = product.querySelector('.subtotal span')
+  //Step 5
+  let subtotal = subtotalDisplay.innerHTML = `${subtotalValue}`
+
+  return subtotalValue;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let totalPrice = 0;
+  const allProductsFuncCall = [...document.getElementsByClassName('product')];
+  allProductsFuncCall.forEach((el) => {
+    updateSubtotal(el);
+  });
+  allProductsFuncCall.forEach((el) => {
+    totalPrice += updateSubtotal(el);
+  });
+  console.log(totalPrice)
+  const totalInHtml = document.querySelector('#total-value span');
+  totalInHtml.innerHTML = `${totalPrice.toFixed(2)}`;
 
-  // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+
+
+
 }
+
+// 
 
 // ITERATION 4
 
